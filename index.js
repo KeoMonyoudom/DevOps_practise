26c7605abc35f8aad2231d65cf63642fe56d0e1b
MohDev24 <keomonyoudom@gmail.com>
2025-09-05 10:34:33 +0700

    commit 2

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, World oudom!');
+    res.send('Hello, World oudom2!');
 });
 
 app.listen(PORT, () => {
